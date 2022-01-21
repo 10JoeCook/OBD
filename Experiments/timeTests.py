2ec365cdcefddfd00492137afe99b63c74a1f3aa
Joe Cook <10joe.cook@gmail.com>
2022-01-21 21:25:42 +0000

    timeTests.py -Some initial time experiments -Test how long connecting will take -Tests to see how long OBD queries will take to execute

---
+++ Experiments/timeTests.py
@@ -0,0 +1,51 @@
+import time
+
+import obd
+
+
+def connection_time_test(attempts: int) -> time:
+    total_time = 0
+    for i in range(attempts):
+        start_time = time.clock()
+        con = obd.OBD()
+        end_time = time.clock()
+        total_time += end_time - start_time
+    return total_time / attempts
+
+
+def query_time_test(test_query: obd.OBDCommand, attempts: int) -> time:
+    con = obd.OBD()
+    total_time = 0
+    for i in range(1000):
+        start_time = time.clock()
+        resp = con.query(test_query)
+        end_time = time.clock()
+        total_time += end_time - start_time
+    return total_time / attempts
+
+
+def speed_multi_query_test(test_query: obd.OBDCommand, queries: int, attempts: int) -> time:
+    con = obd.OBD()
+    total_time = 0
+    for i in range(1000):
+        start_time = time.clock()
+        for j in range(queries):
+            resp = con.query(test_query)
+        end_time = time.clock()
+        total_time += end_time - start_time
+    return total_time / attempts
+
+
+ATTEMPTS = 10
+NO_QUERIES = 10
+print("-----Connection Time-----")
+print("Connection Time:", connection_time_test(ATTEMPTS))
+print("-----RPM Results-----")
+print("Single: ", query_time_test(obd.commands.RPM, ATTEMPTS))
+print("Multi", speed_multi_query_test(obd.commands.RPM, NO_QUERIES, ATTEMPTS))
+print("-----Speed Results-----")
+print("Single: ", query_time_test(obd.commands.SPEED, ATTEMPTS))
+print("Multi", speed_multi_query_test(obd.commands.SPEED, NO_QUERIES, ATTEMPTS))
+print("-----Oil Temp Results-----")
+print("Single: ", query_time_test(obd.commands.OIL_TEMP, ATTEMPTS))
+print("Multi", speed_multi_query_test(obd.commands.OIL_TEMP, NO_QUERIES, ATTEMPTS))
